8d4c022471383da5a260da7913db0933584dc424
Ambika81 <ambikasyam31@gmail.com>
2021-12-11 11:36:53 +0530

    git commit

--- models/snowflake_sample_data/snowflake_sample_data_call_center.sql
+++ models/snowflake_sample_data/snowflake_sample_data_call_center.sql
@@ -1,1 +1,14 @@
-select * from customer
+with customer_new as (
+    select cust_id, cust_name 
+    from customer
+    )
+
+ select
+    customer_new.cust_id,
+    customer_new.cust_name,
+    order_data.order_date
+    from customer_new
+    left join order_data using(cust_id)
+
+
+
