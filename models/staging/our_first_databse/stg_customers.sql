4415f3bf8a3717aa2e6d6b8d5e936bb6117fc98b
ambika-anjane <96051172+ambika-anjane@users.noreply.github.com>
2023-09-21 22:52:52 +0530

    Update stg_customers.sql

--- models/staging/our_first_databse/stg_customers.sql
+++ models/staging/our_first_databse/stg_customers.sql
@@ -1,5 +1,19 @@
-select
-    customerID,
-    Customer_Name,
-    Contact_Name
-    from {{ source('main','Customers') }}
+
+
+with source as (
+    select * from "main"."main"."Customers"
+
+),
+
+renamed as (
+
+    select
+       customerID,
+       Customer_Name,
+       Contact_Name
+
+    from source
+
+)
+
+select * from renamed
